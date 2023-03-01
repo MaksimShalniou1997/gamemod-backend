e4c135254510b8d8dc9ad003f80026d27ab4e22f
Maksim <maksim.shalniou@innowise-group.com>
2023-03-01 10:45:20 +0300

    refactoring

--- src/lib/middlewares/error.middleware.ts
+++ src/lib/middlewares/error.middleware.ts
@@ -4,7 +4,7 @@ import { HttpException } from '../exceptions/HttpException';
 const errorMiddleware = (err: HttpException, req: Request, res: Response, next: NextFunction) => {
 	try {
 		const status: number = err.status || 500;
-		const message: string = err.message || 'Something went wrong';
+		const message: string = err.message || 'Something went wrong!';
 
 		console.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
 		res.status(status).json({ message });
